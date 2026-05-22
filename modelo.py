28da4da55c18384c746bebf1d70b5384509f43a8
Launar07 <laura.naranjor@campusucc.edu.co>
2026-05-22 15:36:43 -0500

    Update file paths to use 'Datos' directory

--- modelo.py
+++ modelo.py
@@ -4,14 +4,14 @@ import streamlit as st
 @st.cache_data
 def cargar_datos_mortalidad():
     """Carga y estandariza el archivo principal de defunciones sin fetos"""
-    df = pd.read_excel("datos/Anexo1.NoFetal2019_CE_15-03-23 (1).xlsx", sheet_name="No_Fetales_2019")
+    df = pd.read_excel("Datos/Anexo1.NoFetal2019_CE_15-03-23 (1).xlsx", sheet_name="No_Fetales_2019")
     df.columns = df.columns.str.upper().str.strip()
     return df
 
 @st.cache_data
 def cargar_catalogo_causas():
     """Carga y limpia el catálogo CIE-10 usando las posiciones físicas de las columnas"""
-    df = pd.read_excel("datos/Anexo2.CodigosDeMuerte_CE_15-03-23.xlsx")
+    df = pd.read_excel("Datos/Anexo2.CodigosDeMuerte_CE_15-03-23.xlsx")
     
     # Usar la fila 9 (índice 8) como nombres de columnas
     titulos = df.iloc[7, [2, 3, 4, 5]].astype(str).str.upper().str.strip().tolist()
@@ -28,6 +28,6 @@ def cargar_catalogo_causas():
 @st.cache_data
 def cargar_datos_divipola():
     """Carga y estandariza los datos geográficos de Divipola"""
-    df = pd.read_excel("datos/Divipola_CE_.xlsx", sheet_name="Hoja1")
+    df = pd.read_excel("Datos/Divipola_CE_.xlsx", sheet_name="Hoja1")
     df.columns = df.columns.str.upper().str.strip()
     return df
